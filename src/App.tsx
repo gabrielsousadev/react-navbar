1f4f3b85b1bf51d58d36566c864257589ede784f
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-18 14:26:56 -0300

    Routes app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Container from './Layout/Container';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Products from './pages/Products';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Container/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/services" element={<Services/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
