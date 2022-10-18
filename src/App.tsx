0e7f9cef6ef93c7a56504d593bda65a85b2a6a75
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-18 14:13:01 -0300

    Pages and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Container from './Layout/Container';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Container/>
     </div>
   );
 }
---
+++ src/Layout/Container.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Home from '../pages/Home';
+
+class Container extends Component {
+  render() {
+    return (
+        <Home/>
+    )
+  }
+}
+
+export default Container;
