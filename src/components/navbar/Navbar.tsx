1fe3c9c77f95635f0cc03a12747456540d8abe7d
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-18 11:22:01 -0300

    Icon react

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,31 @@
-import React, { Component } from 'react'
-import { menuItems } from './MenuItems';
-import './Navbar.css'
+import React, { Component } from "react";
+import { menuItems } from "./MenuItems";
+import { DiReact } from 'react-icons/di';
+import "./Navbar.css";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbarItems">
         <h1 className="navbarLogo">React</h1>
+        <DiReact color="white" size="35px"/>
         <div className="menuIcon">
-            
+
         </div>
         <ul>
-            {menuItems.map((item, index) => {
-                return (
-                    <li key={index}>
-                        <a className={item.cName} href={item.url}>
-                            {item.title}
-                        </a>
-                    </li>
-                )
-            })}
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
