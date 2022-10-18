b36d49c259830c138fe5ddef0d977e2f48ee1b6b
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-18 10:45:15 -0300

    Navbar html base

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbarItems">
+        <h1 className="navbarLogo">React</h1>
+        <div className="menuIcon">
+            
+        </div>
+        <ul>
+            <li><a href="#"></a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
