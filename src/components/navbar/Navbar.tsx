7b2e0bbc14dc213778f2540dac1bcb63bda61993
Recriando Games <gabriel.mattos17@hotmail.com>
2022-10-18 11:42:29 -0300

    Icon hamburguer : close

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
+import { ReactElement } from "react";
 import { menuItems } from "./MenuItems";
 import { DiReact } from 'react-icons/di';
 import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+
 
 class Navbar extends Component {
+
+    state = {clicked:false}
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
   render() {
     return (
       <nav className="navbarItems">
         <h1 className="navbarLogo">React</h1>
         <DiReact color="white" size="35px"/>
-        <div className="menuIcon">
-
+        <div className="menuIcon" onClick={this.handleClick}>
+            <i>{!this.state.clicked ? <GiHamburgerMenu/> : <GrClose/>}</i>
         </div>
         <ul>
           {menuItems.map((item, index) => {
